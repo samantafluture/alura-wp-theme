10fe32837f77d30aab8f138f8e58137024409633
Samanta Fluture <samantafluture@gmail.com>
2021-08-07 14:30:33 -0300

    add styles to header

--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php
 
+function alura_adicionando_recursos_ao_tema(){
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'alura_adicionando_recursos_ao_tema');
+
 function alura_registrando_menu(){
     register_nav_menu( 
         'menu-navegacao',
